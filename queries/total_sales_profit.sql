545914421ed98cc579a4fa1b344d25028dd225c3
macblair221 <160116655+macblair221@users.noreply.github.com>
2025-12-04 20:19:21 -0800

    Update total_sales_profit.sql

--- queries/total_sales_profit.sql
+++ queries/total_sales_profit.sql
@@ -1,1 +1,5 @@
-
+-- Total Sales and Total Profit across the entire dataset
+SELECT 
+    ROUND(SUM(Sales), 2) AS total_sales,
+    ROUND(SUM(Profit), 2) AS total_profit
+FROM `workspace`.`default`.`sample_superstore`;
